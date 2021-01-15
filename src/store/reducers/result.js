72668595fbeea1b891f2c7880bd29bd298e8ceed
Nicholas Jacob <nicholas.jacob@guitarcenter.com>
2021-01-15 14:48:22 -0800

    Using utility functions

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility.js';
 
 const initialState = {
     results: []
@@ -7,19 +8,13 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result * 2})
-            }
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})});
         case actionTypes.DELETE_RESULT:
             // const id = 2;
             // const newArray = [...state.results];
             // newArray.splice(id, 1);
             const updatedArray = state.results.filter(result => result.id !== action.resultElId); // action.resultElId is only passed as a prop to the 'DELETE_RESULT' case where it's equal to result.id and therefore the condition returns false (and thereby the current item is filtered from the array). In all other cases, action.resultElId will be undefined and therefore the condition would return true (so the current item is unaffected)
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return updateObject(state, {results: updatedArray});
     }
     return state;
 }
